23a85994146fdaa4d869147230cc8a1a29c2b06f
the1Riddle <elvisotienomboya@gmail.com>
2023-10-03 13:05:02 -0700

    Call me maybe

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # regular expression must match a 10 digit phone number
-match = ARGV[0].scan(/^\d{10,10}/)
+match = ARGV[0].scan(/^\d{10,10}$/)
 puts match.empty? ? "" : match.join
