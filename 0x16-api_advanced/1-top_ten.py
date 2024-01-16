f23bd579a693cda8ae0ffa52d553b04263fe58a1
the1Riddle <elvisotienomboya@gmail.com>
2024-01-16 19:55:06 +0300

    the top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-   prints the titles of the first 10 hot posts listed for a given subreddit 
+   prints the titles of the first 10 hot posts listed for a given subreddit
 """
 
 import requests
