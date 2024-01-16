53e43ca85d71e7694b7eb14697781a6ae9f7de6b
the1Riddle <elvisotienomboya@gmail.com>
2024-01-16 19:53:27 +0300

    the top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,23 +2,23 @@
 """
    prints the titles of the first 10 hot posts listed for a given subreddit 
 """
+
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
-    """
-        returns the top ten posts for a given subreddit
-    """
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
-    try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
-        print(None)
+    """Function that fetches top 10 gists"""
+    apiUrl = "https://reddit.com/r/{}/hot.json".format(subreddit)
+    userAgent = "Mozilla/5.0"
+    limits = 10
 
-
-if __name__ == "__main__":
-    top_ten(argv[1])
+    response = requests.get(
+        apiUrl, headers={"user-agent": userAgent}, params={"limit": limits})
+    if not response:
+        print('None')
+        return
+    response = response.json()
+    list_obj = response['data']['children']
+    for obj in list_obj:
+        print(obj['data']['title'])
+    return
