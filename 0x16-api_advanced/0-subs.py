2c09b0421f9c5a38c2f18e4eadb4ff314b92ba11
the1Riddle <elvisotienomboya@gmail.com>
2024-01-16 20:01:42 +0300

    returns the number of subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,15 +12,11 @@ def number_of_subscribers(subreddit):
         Function that returns the numbers of
         subscribers of a subreddit passed to it
     """
-
-    apiUrl = "https://reddit.com/r/{}/about.json".format(subreddit)
-    userAgent = "Mozilla/5.0"
-
-    response = requests.get(apiUrl, headers={"user-agent": userAgent})
-    if not response:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    data = response.json()
+    if response.status_code != 200:
         return 0
-    retValue = response.json().get('data').get('subscribers')
-    if retValue:
-        return retValue
     else:
-        return 0
+        return data['data']['subscribers']
