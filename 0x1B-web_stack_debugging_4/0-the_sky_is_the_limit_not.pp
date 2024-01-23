93921704951737ba93d2990cd58445ac76c25346
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2024-01-23 11:24:46 +0300

    Sky is the limit, let's bring that limit higher
    In this web stack debugging task, we are testing how well our web server setup featuring Nginx is doing under pressure ,
    
    Apache Bench is used which is a quite popular tool in the industry. It allows you to simulate HTTP requests to a web server. In this case, I will make 2000 requests to my server with 100 requests at a time.

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# a script that fix and allow unlimited traffic/requests
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
